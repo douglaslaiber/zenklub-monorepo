fd78bc1765b521cc89e914318d8cfa89369cfa34
Eduardo Vargas <eduardo704@gmail.com>
2020-12-12 11:43:35 -0300

    fix env

--- apps/zenklub/src/environments/environment.prod.ts
+++ apps/zenklub/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  api: 'https://zenklub-api-edu.herokuapp.com/api'
 };
--- libs/partner/src/lib/partner.service.ts
+++ libs/partner/src/lib/partner.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Partner } from '@zenklub/data';
-
+import { environment } from '../../../../apps/zenklub/src/environments/environment';
 @Injectable({ providedIn: 'root' })
 export class PartnerService {
+  api = environment.api;
   constructor(private httpClient: HttpClient) {}
 
   getHours() {
-    return this.httpClient.get<string[]>('api/hours');
+    return this.httpClient.get<string[]>(this.api + '/hours');
   }
   getPartners() {
-    return this.httpClient.get<Partner[]>('api/partner');
+    return this.httpClient.get<Partner[]>(this.api + '/partner');
   }
 }
